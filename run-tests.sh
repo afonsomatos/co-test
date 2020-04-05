ca1da65ced5db88f35826e1eba33520826803bc7
Afonso Matos <afonsolfmatos@gmail.com>
2020-04-05 17:24:42 +0100

    fix script

--- run-tests.sh
+++ run-tests.sh
@@ -6,7 +6,10 @@ exe="minor"
 # Go to source directory and make compiler
 cd $srcDir
 srcDir=$(pwd)
-(make clean && make) 2>&1 | grep "conflict"
+{
+make clean
+make
+}  2>&1 | grep "conflict"
 
 # Check conflicts
 if [ $? -eq 0 ]; then
@@ -25,28 +28,43 @@ fi
 cd - > /dev/null
 cd $testDir
 
+total=0
+pass=0
+
+passed="\e[32mPASSED\e[0m"
+failed="\e[31mFAILED\e[0m"
+
 # These tests should pass
-echo " ===== PASS TESTS ===== "
+echo " ===== PASS TESTS ===== [should compile]"
 for f in $(find test/pass -name "*.min"); do
 	"$srcDir/$exe" < $f &> /dev/null
 	if [ $? -ne 0 ]; then
-		echo -e "\e[31mFAILED\e[0m" $f
+		echo -e $failed $f
 	else
-		echo PASSED $f
+        ((pass++))
+		echo -e $passed $f
 	fi
+    ((total++))
 done
 
 # These tests should fail
-echo " ===== FAIL TESTS ===== "
+echo " ===== FAIL TESTS ===== [should not compile]"
 for f in $(find test/fail -name "*.min"); do
 	"$srcDir/$exe" < $f &>/dev/null
 	if [ $? -ne 0 ]; then
-		echo PASSED $f
+        ((pass++))
+		echo -e $passed $f
 	else
-		echo -e "\e[31mFAILED\e[0m" $f
+		echo -e $failed $f
 	fi
+    ((total++))
 done
+
+echo " ===== STATUS ===== "
+echo passed $pass/$total
 
 # Clean source directory
 cd $srcDir
 make clean > /dev/null
+
+exit $((($total-$pass)?1:0))
